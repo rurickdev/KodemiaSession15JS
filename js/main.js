f62a1bda93cce9184ae953fd7dd45f13af950738
Rurick M. Poisot <skintigthvader@gmail.com>
2019-07-30 22:23:54 -0500

    ADD: excercises 1, 2, 3

--- js/main.js
+++ js/main.js
@@ -1,8 +1,45 @@
-// * Cuantos dias haz vivido?
-// dada tu edad cuantos dias haz vivido
+// * How many days have you lived?
 
-// 365 dias por año
-console.log('365 dias por año: ', 25 * 365, 'dias')
+console.log('365 days for year: ', 25 * 365, 'days')
 
-// 30 dias por mes
-console.log('30 dias por año: ', 30 * 12 * 25, 'dias')
+console.log('30 day for month: ', 30 * 12 * 25, 'days')
+
+// * with a 4GB USB Memory, How many pictures of 3MB of size
+// * could be saved? 
+
+var memorySizeInGB = 4 * 1024
+var pictureSize = 3
+
+console.log(memorySizeInGB / pictureSize)
+
+// * Parking with 22 lots, with space for 3 cars and 2 trucks each lot
+/* 
+  with 130 cars and 57 trucks
+
+  how many lots are full?
+  how many cars are not parked?
+  how many trucks are not parked?
+*/
+
+var parkinglots = 22
+var carsQuantity = 130
+var truckQuantity = 57
+
+var parkedCars = (parkinglots * 3)
+var unparkedCars = carsQuantity - parkedCars
+
+console.log('parked cars', parkedCars)
+console.log('unparked cars', unparkedCars)
+
+var parkedTruks = (parkinglots * 2)
+var unparkedTrucks = truckQuantity - parkedTruks
+
+console.log('parked trucks', parkedTruks)
+console.log('unparked trucks', unparkedTrucks)
+
+console.log('The parking is full? ', ((parkedTruks >= (parkinglots * 2) && (parkedCars >= (parkinglots * 3)))))
+
+
+
+
+
