fb2d7ce5eb3af5fdbed3496a4a8798ddc87391ec
Rurick M. Poisot <skintigthvader@gmail.com>
2019-07-30 21:34:00 -0500

    first eExcersise

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// * Cuantos dias haz vivido?
+// dada tu edad cuantos dias haz vivido
+
+// 365 dias por año
+console.log('365 dias por año: ', 25 * 365, 'dias')
+
+// 30 dias por mes
+console.log('30 dias por año: ', 30 * 12 * 25, 'dias')
