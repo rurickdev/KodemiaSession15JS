aa7d37cc4a531ec5ed99a702aaf978d70b7e2605
Rurick M. Poisot <skintigthvader@gmail.com>
2019-08-02 20:15:43 -0500

    ADD: First and second excercises

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
+//* ------------------- Excercise 1 -------------- *//
+var name = 'Hector'
+var index = name.length - 1
 
+do {
+  console.log(name[name.length - (index + 1)])
+  index--
+} while (index >= 0)
+
+//* ------------------- Excercise 2 -------------- *//
+var name2 = 'rotceH'
+var index2 = name2.length - 1
+var normalicedName = ''
+
+do {
+  normalicedName = `${normalicedName}${name2[index2]}`
+  // normalicedName = normalicedName.concat(name2[index2])
+  index2--
+} while (index2 >= 0)
+
+console.log(normalicedName)
