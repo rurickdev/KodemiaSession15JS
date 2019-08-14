c6c715ed7ad47e6ed8190696cc3be360e435c834
Rurick M. Poisot <skintigthvader@gmail.com>
2019-08-14 09:47:28 -0500

    Uncomment code

--- js/main.js
+++ js/main.js
@@ -1,136 +1,136 @@
-// // * ------ Exercise 1 ------ * //
-// var nodes = [
-//   {
-//     'tag': 'ul',
-//     'children': [
-//       {
-//         'tag': 'li',
-//         'text': 'enjoy'
-//       },
-//       {
-//         'tag': 'li',
-//         'text': 'otra'
-//       }, {
-//         'tag': 'li',
-//         'text': 'cosa'
-//       }, {
-//         'tag': 'li',
-//         'text': 'estudien!'
-//       }
-//     ]
-//   },
-//   {
-//     'tag': 'ul',
-//     'children': [
-//       {
-//         'tag': 'li',
-//         'text': 'enjoy'
-//       },
-//       {
-//         'tag': 'li',
-//         'text': 'otra'
-//       }, {
-//         'tag': 'li',
-//         'text': 'cosa'
-//       }, {
-//         'tag': 'li',
-//         'text': 'estudien!'
-//       }
-//     ]
-//   },
-//   {
-//     'tag': 'ul',
-//     'children': [
-//       {
-//         'tag': 'li',
-//         'text': 'enjoy'
-//       },
-//       {
-//         'tag': 'li',
-//         'text': 'otra'
-//       }, {
-//         'tag': 'li',
-//         'text': 'cosa'
-//       }, {
-//         'tag': 'li',
-//         'text': 'estudien!'
-//       }
-//     ]
-//   },
-//   {
-//     'tag': 'ul',
-//     'children': [
-//       {
-//         'tag': 'li',
-//         'text': 'enjoy'
-//       },
-//       {
-//         'tag': 'li',
-//         'text': 'otra'
-//       }, {
-//         'tag': 'li',
-//         'text': 'cosa'
-//       }, {
-//         'tag': 'li',
-//         'text': 'estudien!'
-//       }
-//     ]
-//   },
-//   {
-//     'tag': 'ul',
-//     'children': [
-//       {
-//         'tag': 'li',
-//         'text': 'enjoy'
-//       },
-//       {
-//         'tag': 'li',
-//         'text': 'otra'
-//       }, {
-//         'tag': 'li',
-//         'text': 'cosa'
-//       }, {
-//         'tag': 'li',
-//         'text': 'estudien!'
-//       }
-//     ]
-//   },
-//   {
-//     'tag': 'ul',
-//     'children': [
-//       {
-//         'tag': 'li',
-//         'text': 'enjoy'
-//       },
-//       {
-//         'tag': 'li',
-//         'text': 'otra'
-//       }, {
-//         'tag': 'li',
-//         'text': 'cosa'
-//       }, {
-//         'tag': 'li',
-//         'text': 'estudien!'
-//       }
-//     ]
-//   },
-// ]
-// const buildNodes = (nodes, parent) => {
-//   nodes.forEach(({ tag, children }) => {
-//     let nodeParent = document.createElement(tag)
-//     children.forEach(({ tag, text }) => {
-//       let node = document.createElement(tag)
-//       let nodeText = document.createTextNode(text)
-//       node.appendChild(nodeText)
-//       nodeParent.appendChild(node)
-//     })
-//     nodeParent.classList.add('styled-list')
-//     parent.appendChild(nodeParent)
-//   })
-// }
+// * ------ Exercise 1 ------ * //
+var nodes = [
+  {
+    'tag': 'ul',
+    'children': [
+      {
+        'tag': 'li',
+        'text': 'enjoy'
+      },
+      {
+        'tag': 'li',
+        'text': 'otra'
+      }, {
+        'tag': 'li',
+        'text': 'cosa'
+      }, {
+        'tag': 'li',
+        'text': 'estudien!'
+      }
+    ]
+  },
+  {
+    'tag': 'ul',
+    'children': [
+      {
+        'tag': 'li',
+        'text': 'enjoy'
+      },
+      {
+        'tag': 'li',
+        'text': 'otra'
+      }, {
+        'tag': 'li',
+        'text': 'cosa'
+      }, {
+        'tag': 'li',
+        'text': 'estudien!'
+      }
+    ]
+  },
+  {
+    'tag': 'ul',
+    'children': [
+      {
+        'tag': 'li',
+        'text': 'enjoy'
+      },
+      {
+        'tag': 'li',
+        'text': 'otra'
+      }, {
+        'tag': 'li',
+        'text': 'cosa'
+      }, {
+        'tag': 'li',
+        'text': 'estudien!'
+      }
+    ]
+  },
+  {
+    'tag': 'ul',
+    'children': [
+      {
+        'tag': 'li',
+        'text': 'enjoy'
+      },
+      {
+        'tag': 'li',
+        'text': 'otra'
+      }, {
+        'tag': 'li',
+        'text': 'cosa'
+      }, {
+        'tag': 'li',
+        'text': 'estudien!'
+      }
+    ]
+  },
+  {
+    'tag': 'ul',
+    'children': [
+      {
+        'tag': 'li',
+        'text': 'enjoy'
+      },
+      {
+        'tag': 'li',
+        'text': 'otra'
+      }, {
+        'tag': 'li',
+        'text': 'cosa'
+      }, {
+        'tag': 'li',
+        'text': 'estudien!'
+      }
+    ]
+  },
+  {
+    'tag': 'ul',
+    'children': [
+      {
+        'tag': 'li',
+        'text': 'enjoy'
+      },
+      {
+        'tag': 'li',
+        'text': 'otra'
+      }, {
+        'tag': 'li',
+        'text': 'cosa'
+      }, {
+        'tag': 'li',
+        'text': 'estudien!'
+      }
+    ]
+  },
+]
+const buildNodes = (nodes, parent) => {
+  nodes.forEach(({ tag, children }) => {
+    let nodeParent = document.createElement(tag)
+    children.forEach(({ tag, text }) => {
+      let node = document.createElement(tag)
+      let nodeText = document.createTextNode(text)
+      node.appendChild(nodeText)
+      nodeParent.appendChild(node)
+    })
+    nodeParent.classList.add('styled-list')
+    parent.appendChild(nodeParent)
+  })
+}
 
-// const parent = document.getElementById('container')
-// buildNodes(nodes, parent)
+const parent = document.getElementById('container')
+buildNodes(nodes, parent)
 
 // * ------ Exercise 2 ------ * //
 
